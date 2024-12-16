96cc8ce3d26c944fb4be44aecdd4d6d0e17aba29
jqjel <jelenes@umich.edu>
2024-12-16 00:11:51 -0500

    Add files via upload

---
+++ hello_and_welcome.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, and welcome to C++ in ENGR 101! Compile and then run this code in your terminal." << endl;
+    cout << "Then save a screenshot of your terminal that shows your filepath and this message as Lab07_message.png"<<endl;
+    cout << "and upload in Prairie Learn." << endl;
+
+}
